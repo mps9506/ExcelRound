36777ee286247627c276e62d6c81cf0e060784f7
mps9506 <mpschramm@gmail.com>
2020-01-28 14:32:10 -0600

    add error messages

--- R/xll_round.R
+++ R/xll_round.R
@@ -11,6 +11,9 @@
 #' @examples
 #' xl_round(.5 + -2:4)
 xl_round <- function(x, digits = 0) {
+
+  if (!is.numeric(x)) stop("x must be a numeric argument")
+
   x = x + abs(x) * sign(x) * .Machine$double.eps
   round(x, digits = digits)
 }
